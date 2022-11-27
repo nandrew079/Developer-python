a5be285603ac463a26646900e8a50d31a3738209
nandrew079 <nandrew079@gmail.com>
2022-11-27 23:15:46 +0300

    -dz 1

--- Novikov_Andrey_dz1/task_1_1.py
+++
@@ -1,8 +0,0 @@
-num_day = int(input('Enter the day of the week as a number: '))
-
-if num_day == 6 or num_day == 7:
-    print('yes')
-elif not (1 <= num_day <= 7):
-    print('error')
-else:
-    print('no')
--- Novikov_Andrey_dz1/task_1_2.py
+++
@@ -1,8 +0,0 @@
-for x in [0, 1]:
-    for y in [0, 1]:
-        for z in [0, 1]:
-            if not(x or y or z) == (not x and not y and not z):
-                result = True
-            else:
-                result = False
-            print(f'X={x} Y={y} Z={z} ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z -> {result}')
